b443fb58f72aa91b7c724ac66299dbbf28197245
Urfan00 <urfanagazade75@gmail.com>
2023-09-18 16:31:50 +0400

    Admin&Service

--- TexnoLab/urls.py
+++ TexnoLab/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('Admin.urls')),
     path('', include('Account.urls')),
     path('', include('Blog.urls')),
     path('', include('Core.urls')),
